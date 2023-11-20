9a25d4b19b47b94881f07f290c6fa70efa22ed6e
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-11-20 11:59:24 -0300

    Initial commit

---
+++ src/styles/appColors.js
@@ -0,0 +1,12 @@
+export const colors = StyleSheet.create({
+    black: { color: "#2B2B2B" },
+    semi_black: { color: "#282828" },
+    white: { color: "#FFFFFFFF" },
+    semi_white: { color: "#D8D8D8" },
+    text_semi_white: { color: "#E4E4E4" },
+    mint_green_primary: { color: "#A1E3AF" },
+    mint_green_secondary: { color: "#65C393" },
+    mint_green_dark: { color: "#29A276" },
+    yellow: { color: "#FFD52D" },
+    red: { color: "#D33737" },
+})
