71ff3882b4a051436e2606f70c03be27b806a92c
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-11-21 14:19:14 -0300

    developing Start Screen

--- src/app/components/splash.tsx
+++ src/app/components/splash.tsx
@@ -1,18 +1,77 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { LinearGradient } from 'expo-linear-gradient'
+import { Roboto_400Regular, useFonts } from '@expo-google-fonts/roboto'
+import { useNavigation } from '@react-navigation/native'
 
-export default function Splash() {
-    
+export default function Splash(props: any) {  
+
+  const [msgTxt, setMsgTxt] = useState('')
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    const stateChange = async () => {
+      setMsgTxt('Carregando...');
+      await new Promise(resolve => setTimeout(resolve, 1500));
+      setMsgTxt('Um Minuto...');
+      await new Promise(resolve => setTimeout(resolve, 1500));
+      setMsgTxt('Quase lá...');
+      await new Promise(resolve => setTimeout(resolve, 1500));
+      setMsgTxt('Pronto!');
+      await new Promise(resolve => setTimeout(resolve, 1500));
+      navigation.navigate('Start' as never)
+    }
+    stateChange();
+  }, []);
+
+  let [fontsLoaded, fontError] = useFonts({
+    Roboto_400Regular,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>splash</Text>
-    </View>
+    <LinearGradient colors={["#A1E3AF", "#FFFFFFFF"]} style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.imageView}>
+        <Image style={{ width: 170, height: 170 }} source={require('../../../assets/png/logo_empresa_circle.png')} />
+        <Text style={styles.textStyle}>Tooth Wallet</Text>        
+      </View>
+      <View style={styles.textView}>
+          <ActivityIndicator size="large" color="#2E7D32" />
+          <Text style={{fontSize: 20, color: '#282828', marginTop: 20}}>{msgTxt}</Text>      
+      </View>      
+    </LinearGradient>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#B9FFCA',
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: '#B9FFCA',
+  },
+  imageView: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 20,
+    marginBottom: '60%'
+  },
+  textView: {    
+    gap: 15, 
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle:{
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 30,    
+    color: '#2E7D32',            
+  }
 })
--- src/app/views/Init.tsx
+++ src/app/views/Init.tsx
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 import { Styles } from '../../styles/Styles'
+import { colors } from '../../styles/appColors'
 
 export default function Init() {
   return (
-    <LinearGradient colors={["#A1E3AF", "#65C393", "#29A276"]} style={Styles.cellphoneDialog}>
-      <Text>Init</Text>
+    <LinearGradient colors={["#B9FFCA", "#EAEAEA"]} style={styles.container}>
+      <View style={{ justifyContent: 'center', display: 'flex', alignItems: 'center' }}>
+        <LinearGradient colors={["#A1E3AF", "#65C393", "#29A276"]} style={Styles.cellphoneDialog}>
+          <View style={{ justifyContent: 'center', display: 'flex', alignItems: 'center' }}>
+            <Image style={{ width: 230, height: 260, marginTop: 10 }} source={require('../../../assets/png/begin_box_phone.png')} />
+          </View>
+          <View>
+            <Text style={[colors.white, styles.bigTXT]}> Finanças {"\n"} Pessoais Tão {"\n"} Simples {"\n"} Quanto um {"\n"}<Text style={[colors.yellow]}> Sorriso</Text></Text>
+          </View>
+        </LinearGradient>
+      </View>
     </LinearGradient>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: '#B9FFCA',
+  },
+  bigTXT: {
+    fontSize: 30,
+  }
+})
